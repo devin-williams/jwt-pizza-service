eba9aadbd5ccf0db17e289efde997201c174cc5d
Devin Williams <dcw003@byu.edu>
2025-11-06 13:43:56 -0700

    update order

--- src/service.js
+++ src/service.js
@@ -9,8 +9,8 @@ const metrics = require('./metrics.js');
 
 const app = express();
 app.use(express.json());
-app.use(metrics.requestTracker);
 app.use(setAuthUser);
+app.use(metrics.requestTracker);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
