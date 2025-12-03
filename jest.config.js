ef94073749fb0eb16934dbcc601637bd74ed5b1c
Devin Williams <dcw003@byu.edu>
2025-12-02 19:19:34 -0700

    test

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,31 @@
 module.exports = {
-  testEnvironment: 'node',
-  coverageDirectory: 'coverage',
+  testEnvironment: "node",
+  coverageDirectory: "coverage",
   collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/index.js', // Entry point, minimal logic
-    '!src/config.js', // Configuration file
-    '!src/version.json',
-    '!src/__tests__/**',
-    '!src/__tests__/__mocks__/**',
-    '!src/database/**', // Database is mocked in tests
-    '!src/model/**', // Model is basic and mocked
-    '!src/init.js' // Initialization script
+    "src/**/*.js",
+    "!src/index.js", // Entry point, minimal logic
+    "!src/config.js", // Configuration file
+    "!src/version.json",
+    "!src/__tests__/**",
+    "!src/__tests__/__mocks__/**",
+    "!src/database/**", // Database is mocked in tests
+    "!src/model/**", // Model is basic and mocked
+    "!src/init.js", // Initialization script
   ],
-  coverageReporters: ['text', 'lcov', 'html', 'json-summary'],
+  coverageReporters: ["text", "lcov", "html", "json-summary"],
   coverageThreshold: {
     global: {
-      branches: 77,
+      branches: 75,
       functions: 80,
       lines: 80,
-      statements: 80
-    }
+      statements: 80,
+    },
   },
-  testMatch: [
-    '**/__tests__/**/*.test.js',
-    '**/?(*.)+(spec|test).js'
-  ],
+  testMatch: ["**/__tests__/**/*.test.js", "**/?(*.)+(spec|test).js"],
   testPathIgnorePatterns: [
-    '/node_modules/',
-    'src/__tests__/setup.js',
-    'src/__tests__/__mocks__/'
+    "/node_modules/",
+    "src/__tests__/setup.js",
+    "src/__tests__/__mocks__/",
   ],
-  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.js']
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.js"],
 };
